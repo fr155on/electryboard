c353661d9d2bb36efe6066da5b78ed9e843a4fab
fr155on <prateekchokhandre@gmail.com>
2019-09-27 17:59:05 +0530

    Change Port Env strings

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/electricityDB", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect("mongodb://project:project3@ds023624.mlab.com:23624/heroku_gx9t0hbh" || "mongodb://localhost:27017/electricityDB", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -66,7 +66,7 @@ app.get("/fetch", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 
 });
